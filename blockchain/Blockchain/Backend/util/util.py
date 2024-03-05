160331ef61f6a3f4fd7734f7dcafc0215e134c7d
Mubasheer Shaikh <150155839+MUBIUS@users.noreply.github.com>
2024-03-05 21:08:51 +0530

    Add files via upload
    added private and public address

--- blockchain/Blockchain/Backend/util/util.py
+++ blockchain/Blockchain/Backend/util/util.py
@@ -1,7 +1,13 @@
 import hashlib
+from Crypto.Hash import RIPEMD160
+from hashlib import sha256
 
 def hash256(s):
 
     """TWO ROUNDS OF SHA256"""
     
     return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+def hash160(s):
+    
+    return RIPEMD160.new(sha256(s).digest()).digest()
