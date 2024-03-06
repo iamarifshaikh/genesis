7384f4a967fb5262acd3be86154477aae25c695a
iamarifshaikh <arifamanuddinshaikh@gmail.com>
2024-03-06 11:26:55 +0530

    created_a_file_system_for_storing_blockchain

--- Blockchain/backend/core/blockchain.py
+++ Blockchain/backend/core/blockchain.py
@@ -4,16 +4,24 @@ sys.path.append('/blockchain')
 from Blockchain.backend.core.block import Block
 from Blockchain.backend.core.blockheader import Blockheader
 from Blockchain.backend.util.util import hash256
+from Blockchain.backend.core.database.database import BlockchainDB
+
 import time
-import json
 
 ZERO_HASH = '0' * 64
 VERSION = 1
 
 class Blockchain:
     def __init__(self):
-        self.chain = []
         self.GenesisBlock()
+
+    def writeOnDisk(self,block):
+        blockchainDB = BlockchainDB()
+        blockchainDB.write(block)
+
+    def fetchLastBlock(self):
+        blockchainDB = BlockchainDB()
+        return blockchainDB.lastBlock()
 
     def GenesisBlock(self):
         BlockHeight = 0
@@ -27,14 +35,14 @@ class Blockchain:
         bits = 'ffff001f'
         blockheader = Blockheader(VERSION,prevBlockHash,merkleRoot,timestamp,bits)
         blockheader.mine()
-        self.chain.append(Block(BlockHeight,1,blockheader.__dict__,1,Transaction).__dict__)
-        print(json.dumps(self.chain,indent = 4))
+        self.writeOnDisk([Block(BlockHeight,1,blockheader.__dict__,1,Transaction).__dict__])
+        # print(json.dumps(self.chain,indent = 4))
 
     def main(self):
         while True:
-            lastBlock = self.chain[::-1]
-            BlockHeight = lastBlock[0]["Height"] + 1
-            prevBlockHash = lastBlock[0]["BlockHeader"]["blockHash"]
+            lastBlock = self.fetchLastBlock()
+            BlockHeight = lastBlock["Height"] + 1
+            prevBlockHash = lastBlock["BlockHeader"]["blockHash"]
             self.addBlock(BlockHeight,prevBlockHash)
 
 if __name__ == "__main__":
